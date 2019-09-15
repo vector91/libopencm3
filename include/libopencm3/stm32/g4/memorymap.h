269b42a957899e3eefe6c400404f5bf26c8ab8dd
vector <ettore.zaffaroni@gmail.com>
2019-09-15 10:05:39 +0200

    stm32g4: added memorymap and included it in the common header

---
+++ include/libopencm3/stm32/g4/memorymap.h
@@ -0,0 +1,163 @@
+/*
+ * This file is part of the libopencm3 project.
+ *
+ * Copyright (C) 2019 Ettore Zaffaroni
+ *
+ * This library is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this library.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#ifndef LIBOPENCM3_MEMORYMAP_H
+#define LIBOPENCM3_MEMORYMAP_H
+
+#include <libopencm3/cm3/memorymap.h>
+
+/* --- STM32G4 specific peripheral definitions ----------------------------- */
+
+/* Memory map for all busses */
+#define PERIPH_BASE			(0x40000000U)
+#define PERIPH_BASE_APB1		(PERIPH_BASE + 0x00000)
+#define PERIPH_BASE_APB2		(PERIPH_BASE + 0x10000)
+#define PERIPH_BASE_AHB1		(PERIPH_BASE + 0x20000)
+#define PERIPH_BASE_AHB2		(PERIPH_BASE + 0x8000000)
+#define PERIPH_BASE_AHB3		(0x60000000U)
+
+/* Register boundary addresses */
+
+/* APB1 */
+#define TIM2_BASE			(PERIPH_BASE_APB1 + 0x0000)
+#define TIM3_BASE			(PERIPH_BASE_APB1 + 0x0400)
+#define TIM4_BASE			(PERIPH_BASE_APB1 + 0x0800)
+#define TIM5_BASE			(PERIPH_BASE_APB1 + 0x0C00)
+#define TIM6_BASE			(PERIPH_BASE_APB1 + 0x1000)
+#define TIM7_BASE			(PERIPH_BASE_APB1 + 0x1400)
+/* PERIPH_BASE_APB1 + 0x1800 (0x4000 1800 - 0x4000 1FFF): Reserved */
+#define CRS_BASE			(PERIPH_BASE_APB1 + 0x2000)
+#define TAMP_BASE			(PERIPH_BASE_APB1 + 0x2400)
+#define RTC_BASE			(PERIPH_BASE_APB1 + 0x2800)
+#define WWDG_BASE			(PERIPH_BASE_APB1 + 0x2C00)
+#define IWDG_BASE			(PERIPH_BASE_APB1 + 0x3000)
+/* PERIPH_BASE_APB1 + 0x3400 (0x4000 3400 - 0x4000 37FF): Reserved */
+#define SPI2_BASE			(PERIPH_BASE_APB1 + 0x3800)
+#define SPI3_BASE			(PERIPH_BASE_APB1 + 0x3C00)
+/* PERIPH_BASE_APB1 + 0x4000 (0x4000 4000 - 0x4000 43FF): Reserved */
+#define USART2_BASE			(PERIPH_BASE_APB1 + 0x4400)
+#define USART3_BASE			(PERIPH_BASE_APB1 + 0x4800)
+#define UART4_BASE			(PERIPH_BASE_APB1 + 0x4C00)
+#define UART5_BASE			(PERIPH_BASE_APB1 + 0x5000)
+#define I2C1_BASE			(PERIPH_BASE_APB1 + 0x5400)
+#define I2C2_BASE			(PERIPH_BASE_APB1 + 0x5800)
+#define USB_DEV_FS_BASE		(PERIPH_BASE_APB1 + 0x5c00)
+#define USB_SRAM_BASE		(PERIPH_BASE_APB1 + 0x6000)
+#define FDCAN1_BASE			(PERIPH_BASE_APB1 + 0x6400)
+#define FDCAN2_BASE			(PERIPH_BASE_APB1 + 0x6800)
+#define FDCAN3_BASE			(PERIPH_BASE_APB1 + 0x6C00)
+#define PWR_BASE			(PERIPH_BASE_APB1 + 0x7000)
+/* PERIPH_BASE_APB1 + 0x7400 (0x4000 7400 - 0x4000 77FF): Reserved */
+#define I2C3_BASE			(PERIPH_BASE_APB1 + 0x7800)
+#define LPTIM1_BASE			(PERIPH_BASE_APB1 + 0x7C00)
+#define LPUART1_BASE		(PERIPH_BASE_APB1 + 0x8000)
+#define I2C4_BASE			(PERIPH_BASE_APB1 + 0x8400)
+/* PERIPH_BASE_APB1 + 0x8800 (0x4000 8800 - 0x4000 9FFF): Reserved */
+#define UCPD1_BASE			(PERIPH_BASE_APB1 + 0xA000)
+#define FDCAN_RAM_BASE		(PERIPH_BASE_APB1 + 0xA400)
+/* PERIPH_BASE_APB1 + 0xAFFE (0x4000 AFFE - 0x4000 FFFF): Reserved */
+
+/* APB2 */
+#define SYSCFG_BASE			(PERIPH_BASE_APB2 + 0x0000)
+#define VREFBUS_BASE		(PERIPH_BASE_APB2 + 0x0030)
+#define COMP_BASE			(PERIPH_BASE_APB2 + 0x0200)
+#define OPAMP_BASE			(PERIPH_BASE_APB2 + 0x0300)
+#define EXTI_BASE			(PERIPH_BASE_APB2 + 0x0400)
+/* PERIPH_BASE_APB2 + 0x0800 (0x4001 0800 - 0x4001 2BFF): Reserved */
+#define TIM1_BASE			(PERIPH_BASE_APB2 + 0x2C00)
+#define SPI1_BASE			(PERIPH_BASE_APB2 + 0x3000)
+#define TIM8_BASE			(PERIPH_BASE_APB2 + 0x3400)
+#define USART1_BASE			(PERIPH_BASE_APB2 + 0x3800)
+#define SPI4_BASE			(PERIPH_BASE_APB2 + 0x3C00)
+#define TIM15_BASE			(PERIPH_BASE_APB2 + 0x4000)
+#define TIM16_BASE			(PERIPH_BASE_APB2 + 0x4400)
+#define TIM17_BASE			(PERIPH_BASE_APB2 + 0x4800)
+/* PERIPH_BASE_APB2 + 0x4C00 (0x4001 4C00 - 0x4001 4FFF): Reserved */
+#define TIM17_BASE			(PERIPH_BASE_APB2 + 0x5000)
+#define SAI1_BASE			(PERIPH_BASE_APB2 + 0x5400)
+/* PERIPH_BASE_APB2 + 0x5800 (0x4001 5800 - 0x4001 67FF): Reserved */
+#define HRTIM_BASE			(PERIPH_BASE_APB2 + 0x6800)
+/* PERIPH_BASE_APB2 + 0x7800 (0x4001 7800 - 0x4001 FFFF): Reserved */
+
+/* AHB1 */
+#define DMA1_BASE			(PERIPH_BASE_AHB1 + 0x0000)
+#define DMA2_BASE			(PERIPH_BASE_AHB1 + 0x0400)
+#define DMAMUX_BASE			(PERIPH_BASE_AHB1 + 0x0800)
+#define CORDIC_BASE			(PERIPH_BASE_AHB1 + 0x0C00)
+#define RCC_BASE			(PERIPH_BASE_AHB1 + 0x1000)
+#define FMAC_BASE			(PERIPH_BASE_AHB1 + 0x1400)
+#define FLASH_MEM_INTERFACE_BASE	(PERIPH_BASE_AHB1 + 0x2000)
+/* PERIPH_BASE_AHB1 + 0x2400 (0x4002 2400 - 0x4002 2FFF): Reserved */
+#define CRC_BASE			(PERIPH_BASE_AHB1 + 0x3000)
+/* PERIPH_BASE_AHB1 + 0x2400 (0x4002 2C00 - 0x47FF FFFF): Reserved */
+
+/* AHB2 */
+#define GPIO_PORT_A_BASE		(PERIPH_BASE_AHB2 + 0x0000)
+#define GPIO_PORT_B_BASE		(PERIPH_BASE_AHB2 + 0x0400)
+#define GPIO_PORT_C_BASE		(PERIPH_BASE_AHB2 + 0x0800)
+#define GPIO_PORT_D_BASE		(PERIPH_BASE_AHB2 + 0x0C00)
+#define GPIO_PORT_E_BASE		(PERIPH_BASE_AHB2 + 0x1000)
+#define GPIO_PORT_F_BASE		(PERIPH_BASE_AHB2 + 0x1400)
+#define GPIO_PORT_G_BASE		(PERIPH_BASE_AHB2 + 0x1800)
+/* PERIPH_BASE_AHB2 + 0x1C00 (0x4800 1C00 - 0x4FFF FFFF): Reserved */
+#define ADC1_BASE				(PERIPH_BASE_AHB2 + 0x8000000)
+#define ADC2_BASE				(PERIPH_BASE_AHB2 + 0x8000100)
+#define ADC12_BASE				(PERIPH_BASE_AHB2 + 0x8000300)
+#define ADC3_BASE				(PERIPH_BASE_AHB2 + 0x8000400)
+#define ADC4_BASE				(PERIPH_BASE_AHB2 + 0x8000500)
+#define ADC5_BASE				(PERIPH_BASE_AHB2 + 0x8000600)
+#define ADC34_BASE				(PERIPH_BASE_AHB2 + 0x8000700)
+#define DAC1_BASE				(PERIPH_BASE_AHB2 + 0x8000800)
+#define DAC2_BASE				(PERIPH_BASE_AHB2 + 0x8000C00)
+#define DAC3_BASE				(PERIPH_BASE_AHB2 + 0x8001000)
+#define DAC4_BASE				(PERIPH_BASE_AHB2 + 0x8001400)
+/* PERIPH_BASE_AHB2 + 0x8001800 (0x5000 1800 - 0x5005 FFFF): Reserved */
+#define AES_BASE				(PERIPH_BASE_AHB2 + 0x8060000)
+/* PERIPH_BASE_AHB2 + 0x8060400 (0x5006 0400 - 0x5006 07FF): Reserved */
+#define RNG_BASE				(PERIPH_BASE_AHB2 + 0x8060800)
+/* PERIPH_BASE_AHB2 + 0x8060C00 (0x5006 0C00 - 0x5FFF FFFF): Reserved */
+
+/* AHB3 */
+/* Address: 0x60000000 */
+#define FMC_BANK1			(PERIPH_BASE_AHB3)
+/* Address: 0x80000000 */
+#define FMC_BANK3			(PERIPH_BASE_AHB3 + 0x20000000U)
+/* Address: 0x90000000 */
+#define QUADSPI_BANK		(PERIPH_BASE_AHB3 + 0x30000000U)
+/* Address: 0xA0000000 */
+#define FMC_BASE			(PERIPH_BASE_AHB3 + 0x40000000U)
+/* Address: 0xA0001000 */
+#define QUADSPI_BASE		(PERIPH_BASE_AHB3 + 0x40001000U)
+
+/* PPIB */
+#define DBGMCU_BASE			(PPBI_BASE + 0x00042000)
+
+/* Device Electronic Signature */
+#define DESIG_FLASH_SIZE_BASE		(0x1FFF75E0U)
+#define DESIG_UNIQUE_ID_BASE		(0x1FFF7590U)
+#define DESIG_UNIQUE_ID0		MMIO32(DESIG_UNIQUE_ID_BASE)
+#define DESIG_UNIQUE_ID1		MMIO32(DESIG_UNIQUE_ID_BASE + 4)
+#define DESIG_UNIQUE_ID2		MMIO32(DESIG_UNIQUE_ID_BASE + 8)
+
+/* ST provided factory calibration values @ 3.0V */
+#define ST_VREFINT_CAL		MMIO16(0x1FFF75AA)
+#define ST_TSENSE_CAL1_30C	MMIO16(0x1FFF75A8)
+#define ST_TSENSE_CAL2_110C	MMIO16(0x1FFF75CA)
+
+#endif
